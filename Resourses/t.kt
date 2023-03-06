46bdd07c9c9dce7e5b50d78e4ce35014f5bb579e
minhThang9911 <minhthang0792@gmail.com>
2023-03-06 22:26:31 +0700

    update

--- Resourses/t.kt
+++ Resourses/t.kt
@@ -1,8 +1,6 @@
+import java.net.InetAddress;
+
 fun main(){
-    val name = "Strange.World.2022.2160p.AMZN.WEB-DL.DDP5.1.H.264-FLUX"
-    val indexs = arrayOf(name.indexOf("480p"),name.indexOf("720p"),name.indexOf("1080p"),name.indexOf("2160p"))
-    val index = indexs.max()
-    val out = name.substring(0, index)
-    println(out)
-    println(indexs.toString())
+    val ip = InetAddress.getLocalHost()
+    println("The IP address of the Current Device is: " + ip.getHostAddress())
 }
