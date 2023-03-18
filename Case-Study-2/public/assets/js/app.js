850b911f90c7c37d5d2d523152bbd3c083e054b6
minhThang9911 <minhthang0792@gmail.com>
2023-03-18 19:03:00 +0700

    update

---
+++ Case-Study-2/public/assets/js/app.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+    if (window.innerWidth < 992) {
+        $('.side-menu').addClass('offcanvas offcanvas-end');
+    } else {
+        $('.btn-offcavas').unbind('click')
+    }
+});
+$(window).resize(function () {
+    if (window.innerWidth < 992) {
+        $('.side-menu').addClass('offcanvas offcanvas-end');
+        $('.btn-offcavas').bind('click')
+    } else {
+        $('.side-menu').removeClass('offcanvas offcanvas-end');
+        $('.btn-offcavas').unbind('click')
+    }
+});
+
+function tabcontrol () {
+    $('.tab-khac').toggleClass("active")
+    $('.menu-khac .nav-link').toggleClass("active")
+    $('.tab-chuyen-muc').toggleClass("active")
+    $('.menu-chuyenmuc .nav-link').toggleClass("active")
+}
+
+$('.menu-chuyenmuc').click(tabcontrol)
+
+$('.menu-khac').click(tabcontrol)
+
+
+
+console.log("ssssssssssssss")
